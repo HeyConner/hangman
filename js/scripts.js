108c0666a57a5b954014653bc7b35f659992a375
Conner Belvin and Keir Staple <keirstaple@comcast.net>
2016-01-27 10:28:10 -0800

    random word, word letters functions working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var wordArray = ["apple", "cherry", "banana", "tomato", "strawberry", "watermelon", "grape", "blueberry", "marionberry", "grapefruit", "mango", "pineapple", "peach", "lime", "lemon"]
-// var randomWord = wordArray[Math.floor(Math.random() * myArray.length)];
 
-wordArray.prototype.randomWord = function (){
-  return this[Math.floor(Math.random() * this.length)]
-}
+var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+
+var randomWordLettersArray = randomWord.split()
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,11 @@
 describe("randomWord", function() {
   it("creates a list of words the computer can randomly select from for a new Hangman game ", function() {
-  expect(randomWord()).to.equal.([""])
+  expect(randomWord).to.be.a('string');
+  });
+});
+
+describe("randomWordLettersArray", function() {
+  it("splits the random word string into an array of separate letters", function() {
+  expect(randomWordLettersArray).to.be.a('array');
   });
 });
